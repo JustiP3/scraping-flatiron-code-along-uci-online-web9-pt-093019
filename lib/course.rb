af35d59314d1872977749ef17a9fff5d211920a3
Justin Phillips <justinphil02@yahoo.com>
2019-11-14 00:20:23 +0000

    Automatically backed up by Learn

--- lib/course.rb
+++ lib/course.rb
@@ -2,10 +2,10 @@ class Course
   attr_accessor :title, :schedule, :description
   @@all = []
 
-  def initialize 
-    self.all << self 
-  end 
-  
+  def initialize
+    self.all << self
+  end
+
   def self.all
     self.all
   end
